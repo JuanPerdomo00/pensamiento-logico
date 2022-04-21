db35af0397e0d86e58a0cec43d5c170e7445982d
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-04-21 09:47:05 -0500

    ejemplos en c

---
+++ cosas_c/mayor_numero.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int max_four(int num1, int num2, int num3, int num4) {
+	// maximo de 4
+	int array[4] = {num1,num2,num3,num4};
+	int max = array[0];
+
+	for(int i = 0; i < 4; i++) {
+			if(max < array[i]) {
+				max = array[i];
+
+			}
+
+	}
+
+	return max;
+
+}
+
+
+int main() {
+	int num1, num2, num3, num4;
+	int res = max_four(12,22,78,99);
+
+	printf("%d", res);
+
+	return 0;
+}
